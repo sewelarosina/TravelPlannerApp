6071ab60106fb2582c3953f1c4ded1c41a812e10
sewelarosina <kseakamela08@gmail.com>
2026-01-22 08:23:05 +0200

    Add project files.

---
+++ TravelPlannerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelPlannerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

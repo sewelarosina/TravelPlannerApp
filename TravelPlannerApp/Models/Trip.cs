9794fad9588ac761c1dc44a13370216053921043
sewelarosina <kseakamela08@gmail.com>
2026-01-22 08:40:23 +0200

    created a trip clasd in models folder

---
+++ TravelPlannerApp/Models/Trip.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelPlannerApp.Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Destination { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Budget { get; set; }
+    }
+}
